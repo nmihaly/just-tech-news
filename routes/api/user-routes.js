6ff1c22b461a03e1ee0d9c9e4f1ec0aad6a1452c
Your Name <nickmihaly@gmail.com>
2021-07-07 01:22:32 -0700

    hash passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,13 +55,14 @@ router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
-    User.update(req.body, {
+    User.update(req.body, { 
+        individualHooks: true,
         where: {
             id: req.params.id
         }
     })
-    .then(dbUSerData => {
-        if(!dbUSerData[0]) {
+    .then(dbUserData => {
+        if(!dbUserData[0]) {
             res.status(404).json({ message: 'No user found with this id' });
             return;
         }
